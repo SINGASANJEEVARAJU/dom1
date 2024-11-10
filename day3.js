9f586da3a7f368b7343abc3df6c7146a555eccea
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-11-10 20:49:58 +0530

    files  are added

---
+++ day3.js
@@ -0,0 +1,32 @@
+// let k=document.getElementById("p");
+// k.style.backgroundColor="yellow"
+// k.style.fontSize="50px"
+// k.style.display="inline"
+// console.log(k.textContent);
+// k.textContent="i love prabhas"
+// k.innerHTML=`my goal is to get a job in mnc <span style="background-Color
+// :green;fontSize:70px">surya<span><a href="">youtube</a>raju`
+// k.innerText="i want make my family happpy untill i will dead"
+// console.log(k);
+// let a=document.getElementById("btn")
+// a.onclick=k.textContent="i learnt frontend
+// function b(){
+//     k.style.backgroundColor="red"
+// k.style.fontSize="10px"
+// k.textContent="my goal is to get a job in fullstack developer"
+// k.innerHTML=`I NEED MORE PRACTICE IN JS
+// <img src="https://media.istockphoto.com/id/2180294367/photo/ganesh-sculpture-lord-ganpati-ganesha-on-colourful-background-ganpati-happy-ganesh-chaturthi.jpg?s=612x612&w=0&k=20&c=BYdEtJA567fUujX7eJnURBOf1U8o0VFXGGs32PjLtPM=" width="100px" height="100px"/>`
+// console.log(k)
+// }
+// var c=document.getElementById("btn")
+//  function  c(){
+//     k.style.backgroundColor="red"
+// k.style.fontSize="70px"
+// k.textContent="my goal is to get a job in fullstack developer"
+// k.innerHTML=`get job early
+// <img src="https://media.istockphoto.com/id/2180294367/photo/ganesh-sculpture-lord-ganpati-ganesha-on-colourful-background-ganpati-happy-ganesh-chaturthi.jpg?s=612x612&w=0&k=20&c=BYdEtJA567fUujX7eJnURBOf1U8o0VFXGGs32PjLtPM=" width="400px" height="500px"/>`
+// console.log(k)
+// }
+let p=document.getElementsByClassName("c");
+p.style.backgroundColor="yellow"
+console.log(p[0]);
